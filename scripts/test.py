dcdb071765b75350e42128ec455d42101bd42219
navercm418 <51379254+navercm418@users.noreply.github.com>
2020-11-30 08:04:56 -0500

    made low poly bow

--- scripts/test.py
+++ scripts/test.py
@@ -6,5 +6,4 @@ import sys
 # C:\CodeProjects\bge\Release\coj_game\ -> db\ -> coj_data.db
 # os.chdir("..") go back one
 root = os.path.abspath(os.curdir)
-
 print(os.path.join(root, "db", "coj_data.db")) 
